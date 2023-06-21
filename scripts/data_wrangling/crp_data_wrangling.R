1e96ec2591f57b542cda268275e459db3e8752be
collazoa <anja.collazo@charite.de>
2023-06-21 13:25:53 +0200

    se calculation

--- scripts/data_wrangling/crp_data_wrangling.R
+++ scripts/data_wrangling/crp_data_wrangling.R
@@ -5,7 +5,6 @@
 #preparing crp (Reproducibility Project Cancer Biology) data set 
 # https://osf.io/39s7j
 
-source("./scripts/data_wrangling/load_packages.R")
 crp<-read.csv(file = "./scripts/data_wrangling/crp.csv", header = TRUE, sep = ",")
 
 crp$Original.sample.size<-as.numeric(crp$Original.sample.size)
@@ -19,14 +18,14 @@ crp <- crp[vec_not_NA,]
 
 #selecting relevant columns 
 
-sel <- colnames(crp) %in% c("Original.sample.size","Original.p.value","Original.standard.error",
+sel <- colnames(crp) %in% c("Original.sample.size","Original.p.value",
                             "Original.effect.size", "Replication.effect.size",
                             "Original.lower.CI","Original.upper.CI", 
                             "Replication.sample.size", "Effect.size.type", "Replication.p.value") 
 crp <- crp[,sel]
 
 #assigning standardized names to the tibble
-names<-c("orig_ss", "rep_ss", "orig_p_2sided", "rep_p_2sided", "effect_size_type", "orig_d", "orig_se", "orig_ci_low", "orig_ci_high", "rep_d")
+names<-c("orig_ss", "rep_ss", "orig_p_2sided", "rep_p_2sided", "effect_size_type", "orig_d", "orig_ci_low", "orig_ci_high", "rep_d")
 colnames(crp)<-names
 
 
@@ -34,7 +33,7 @@ colnames(crp)<-names
 # generating dataset for descriptive analysis 
 crp_d <- crp
 
-write.csv(crp_d, "./scripts/data_wrangling/crp_d.R")
+#write.csv(crp_d, "./scripts/data_wrangling/crp_d.R")
 #############################################
 
 
@@ -43,20 +42,17 @@ crp <- crp %>%
   filter(!is.na(orig_d) == TRUE)
 
 crp$project<-"CRP"
-crp$zo <- crp$orig_d/crp$orig_se
 
-# crp$orig_z <- numeric(length = nrow(crp))
-# crp$orig_ci_low_z<-numeric(length = nrow(crp))
-# crp$orig_ci_high_z<-numeric(length = nrow(crp))
-# crp$orig_se_z<-numeric(length = nrow(crp))
-# 
-# for (i in 1:nrow(crp)) {
-#   crp$orig_z[i] <- FisherZ(d_to_r(crp$orig_d[i]))
-#   crp$orig_ci_high_z[i]<-FisherZ(d_to_r(crp$orig_ci_high[i]))
-#   crp$orig_ci_low_z[i]<-FisherZ(d_to_r(crp$orig_ci_low[i]))
-#   crp$orig_se_z[i]<-ci2se(lower = crp$orig_ci_low_z[i], upper = crp$orig_ci_high_z[i])
-# }
-#orig_se is on the z-scale 
+for (i in 1:nrow(crp)) {
+  crp$orig_se[i] <-
+    ci2se(lower = crp$orig_ci_low[i], 
+          upper = crp$orig_ci_high[i])
+  
+  
+  crp$zo[i] <-
+    crp$orig_d[i]/crp$orig_se[i]
+}
+
 
 
 
