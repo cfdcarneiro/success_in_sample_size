72a72e43ff099a12770e24141c53b0f02ab29fe7
collazoa <anja.collazo@charite.de>
2024-01-30 16:16:39 +0100

    decision tree, n = 1000

--- scripts/simulation/sim_approach_b_0.5.R
+++ scripts/simulation/sim_approach_b_0.5.R
@@ -1,22 +1,25 @@
 ###########################################################
 #
-#   safeguard power analysis: 
-#   Replication powered lower 80% confidence interval 
-# 
+#   Replication powered for a SESOI = 0.5 
 #
 ###########################################################
 
 
 
 # source packages and additional functions
-source("../scripts/simulation/functions_for_simulation.R")
-source("../scripts/data_wrangling/load_packages.R")
-
+source("./scripts/simulation/functions_for_simulation.R")
+source("./scripts/data_wrangling/load_packages.R")
 
 # load combined data of all three replication projects
 load("./datasets/df_combined.RData")
+
 # calculate sample size for replication
 
+# Approach B:
+# Replication study powered at 50% for the smallest effect size of interest (SESOI)
+# SESOI 1 = 0.5
+
+# set SESOI to 0.5
 
 # add column study_id to loop over later
 df_combined <-
@@ -24,50 +27,21 @@ df_combined <-
   mutate(study_id = 1:nrow(df_combined)) %>% 
   select(study_id, everything())
 
+SESOI <- 0.5 
 
-# Approach C:
-# Replication study powered at 80% for the lower 80% confidence bound 
-# obtained from the original study
-# if lower CI bound < 0, the rep_sample_size_c column will display NA
-
-# use des() function from compute.es package to compute CI around orig_d
-# ci_80_low is the lower bound of the 80% CI
-
-df_combined$ci_80_low <- NULL
+rep_sample_size_b_0.5 <- NULL
 
 for (i in 1:nrow(df_combined)) {
   
-  df_combined$ci_80_low[i] <-
-    
-    des(d =df_combined$ orig_d[i], 
-        n.1 = df_combined$orig_ss[i] / 2, 
-        n.2 = df_combined$orig_ss[i] / 2,
-        level = 80)$l.d
+  rep_sample_size_b_0.5[i] <-
+    ceiling(sample_size_b(data = df_combined[i, ],
+                          SESOI = SESOI,
+                          power = 0.5))
   
 }
 
-summary(df_combined$ci_80_low)
+df_combined$rep_sample_size_b_0.5 <- rep_sample_size_b_0.5 * 2
 
-# now compute replication sample size with the lower 80% confidence bound
-rep_sample_size_c <- NULL
-
-for (i in 1:nrow(df_combined)) {
-  
-  if (df_combined$ci_80_low[i] > 0) {
-    
-    rep_sample_size_c[i] <-
-      ceiling(sample_size_c(data = df_combined[i, ],
-                            power = 0.8))
-    
-  } else {
-    
-    rep_sample_size_c[i] <- NA
-    
-  }
-  
-}
-
-df_combined$rep_sample_size_c <- rep_sample_size_c * 2
 
 
 ################################
@@ -76,8 +50,8 @@ df_combined$rep_sample_size_c <- rep_sample_size_c * 2
 #
 ################################
 
-vec_not_nec <- ifelse(df_combined$rep_sample_size_c < 4, TRUE, FALSE)
-vec_unfeasible <- ifelse(df_combined$rep_sample_size_c >= 280 | df_combined$ci_80_low < 0, TRUE, FALSE)
+vec_not_nec <- ifelse(df_combined$rep_sample_size_b_0.5 < 4, TRUE, FALSE)
+vec_unfeasible <- ifelse(df_combined$rep_sample_size_b_0.5 > 280 | df_combined$orig_ci_high < SESOI, TRUE, FALSE)
 vec_go <- ifelse(vec_not_nec == FALSE & vec_unfeasible == FALSE, TRUE, FALSE)
 
 vec_conducted <- vector(length = nrow(df_combined))
@@ -87,30 +61,36 @@ vec_conducted[which(vec_not_nec == TRUE)] <- "not_necessary"
 
 df_combined$conducted <- vec_conducted
 
-decision1_c <- 
-  df_combined%>%
-  group_by(conducted)%>%
-  dplyr::summarize(n = n())%>%
-  mutate(approach = "safeguard")
-decision1_c
+decision1_b_0.5 <- df_combined%>%group_by(conducted)%>%summarize(n = n())%>%mutate(approach = "SESOI_0.5")
+decision1_b_0.5
 
+
+####################################
+#
+#
+#     Simulate replication studies for 
+#     chosen experiments 
+#
+#####################################
 
 
 ##################################
 ### Simulate replication study ###
-### SCENARIO 1: M - error ########
+### SCENARIO 1: M-error ##########
 ##################################
-
-
 
 # set seed to reproduce results
 set.seed(84335)
 
 # number of experiments we run for each true underlying effect size
-n_exp <- 3
+n_exp <- 2
 
+# first use replication sample size with 95% power
+
+# for simulation only take significant results
 study_id_vector <- which(df_combined$conducted == "yes")
 
+# first use replication sample size with SESOI = 0.5
 list_rep_data <- 
   
   foreach(study_id = study_id_vector) %do% {
@@ -121,11 +101,11 @@ list_rep_data <-
       
       rep_data[[i]] <- 
         generate_study(df_combined$orig_d[study_id] / 2,
-                       sample_size = df_combined$rep_sample_size_c[study_id])
+                       sample_size = df_combined$rep_sample_size_b_0.5[study_id])
       
       rep_data[[i]] <-
         rep_data[[i]] %>% 
-        mutate(study_id = df_combined$study_id[study_id],
+        mutate(study_id = study_id_vector[study_id],
                ES_true = df_combined$orig_d[study_id] / 2)
       
     }
@@ -133,145 +113,6 @@ list_rep_data <-
     list_rep_data <- rep_data
     
   }
-
-rep_data_summary <- list()
-
-plan(multisession)
-for (i in 1:length(study_id_vector)) {
-  
-  rep_data_summary[[i]] <- 
-    future_map(list_rep_data[[i]], get_summary_study_rep)
-  
-}
-
-row_names <- NULL
-col_names <- c("study_id", "p_value", "effect")
-
-res_summary_rep_c <-
-  as_tibble(matrix(unlist(rep_data_summary),
-                   nrow = n_exp * length(study_id_vector), byrow = TRUE,
-                   dimnames = list(c(row_names),
-                                   c(col_names))))
-
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
-            N = n(),
-            pct_success = n_success/N * 100) %>%
-  mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
-         es_true = df_combined$orig_d[study_id_vector] / 2,
-         sample_size_approach = "c",
-         project = df_combined$project[study_id_vector],
-         scenario = "m_error")
-
-res_summary_c <- left_join(df_combined, res_summary_c)
-
-
-
-save(res_summary_c, file = "./datasets/res_summary_c_m_error.RData")
-
-
-##################################
-### Simulate replication study ###
-### SCENARIO 2: null effect ######
-##################################
-
-set.seed(84335)
-
-list_rep_data <- 
-  
-  foreach(study_id = study_id_vector) %do% {
-    
-    rep_data <- list()
-    
-    for(i in 1:n_exp) {
-      
-      rep_data[[i]] <- 
-        generate_study(0,
-                       sample_size = df_combined$rep_sample_size_c[study_id])
-      
-      rep_data[[i]] <-
-        rep_data[[i]] %>% 
-        mutate(study_id = df_combined$study_id[study_id],
-               ES_true = 0)
-      
-    }
-    
-    list_rep_data <- rep_data
-    
-  }
-
-rep_data_summary <- list()
-
-plan(multisession, workers = 10) 
-for (i in 1:length(study_id_vector)) {
-  
-  rep_data_summary[[i]] <- 
-    future_map(list_rep_data[[i]], get_summary_study_rep)
-  
-}
-# rep_data_summary[[1]]
-
-row_names <- NULL
-col_names <- c("study_id", "p_value", "effect")
-
-res_summary_rep_c <-
-  as_tibble(matrix(unlist(rep_data_summary),
-                   nrow = n_exp * length(study_id_vector), byrow = TRUE,
-                   dimnames = list(c(row_names),
-                                   c(col_names))))
-
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
-            N = n(),
-            pct_success = n_success/N * 100) %>%
-  mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
-         es_true = 0,
-         sample_size_approach = "c",
-         project = df_combined$project[study_id_vector],
-         scenario = "null_effect")
-
-res_summary_c <- left_join(df_combined, res_summary_c)
-
-
-save(res_summary_c, file = "./datasets/res_summary_c_null.RData")
-
-
-##################################
-### Simulate replication study ###
-### SCENARIO 3: s- error #########
-##################################
-
-set.seed(84335)
-
-list_rep_data <- 
-  
-  foreach(study_id = study_id_vector) %do% {
-    
-    rep_data <- list()
-    
-    for(i in 1:n_exp) {
-      
-      rep_data[[i]] <- 
-        generate_study(ES_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]),
-                       sample_size = df_combined$rep_sample_size_c[study_id])
-      
-      rep_data[[i]] <-
-        rep_data[[i]] %>% 
-        mutate(study_id = df_combined$study_id[study_id],
-               ES_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]))
-      
-    }
-    
-    list_rep_data <- rep_data
-    
-  }
-
 
 rep_data_summary <- list()
 
@@ -283,32 +124,191 @@ for (i in 1:length(study_id_vector)) {
   
 }
 
+# rep_data_summary[[1]]
+
 row_names <- NULL
 col_names <- c("study_id", "p_value", "effect")
 
-res_summary_rep_c <-
+res_summary_rep_b_0.5 <- 
   as_tibble(matrix(unlist(rep_data_summary),
                    nrow = n_exp * length(study_id_vector), byrow = TRUE,
                    dimnames = list(c(row_names),
                                    c(col_names))))
 
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
+res_summary_b_0.5 <- 
+  res_summary_rep_b_0.5 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= SESOI),
             N = n(),
-            pct_success = n_success/N * 100) %>%
-  mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
-         es_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]),
-         sample_size_approach = "c",
-         project = df_combined$project[study_id_vector],
+            pct_success = n_success/N * 100) %>% 
+  mutate(orig_ss = df_combined$orig_ss,
+         rep_sample_size = df_combined$rep_sample_size_b_0.5,
+         es_true = df_combined$orig_d / 2,
+         sample_size_approach = "b_0.5",
+         project = df_combined$project,
+         scenario = "m_error")
+
+res_summary_b_0.5 <- left_join(df_combined, res_summary_b_0.5)
+
+
+res_summary_b_0.5_m_error <- res_summary_b_0.5
+
+save(res_summary_b_0.5_m_error, file = "./datasets/res_summary_b_0.5_m_error.RData")
+
+
+##################################
+### Simulate replication study ###
+### SCENARIO 2: Null-effect ######
+##################################
+
+
+
+set.seed(84335)
+
+# first use replication sample size with SESOI = 0.5
+list_rep_data <- 
+  
+  foreach(study_id = study_id_vector) %do% {
+    
+    rep_data <- list()
+    
+    for(i in 1:n_exp) {
+      
+      rep_data[[i]] <- 
+        generate_study(ES_true = 0,
+                       sample_size = df_combined$rep_sample_size_b_0.5[study_id])
+      
+      rep_data[[i]] <-
+        rep_data[[i]] %>% 
+        mutate(study_id = study_id_vector[study_id],
+               ES_true = 0)
+      
+    }
+    
+    list_rep_data <- rep_data
+    
+  }
+
+rep_data_summary <- list()
+
+plan(multisession, workers = 10)
+for (i in 1:length(study_id_vector)) {
+  
+  rep_data_summary[[i]] <- 
+    future_map(list_rep_data[[i]], get_summary_study_rep)
+  
+}
+
+# rep_data_summary[[1]]
+
+row_names <- NULL
+col_names <- c("study_id", "p_value", "effect")
+
+res_summary_rep_b_0.5 <- 
+  as_tibble(matrix(unlist(rep_data_summary),
+                   nrow = n_exp * length(study_id_vector), byrow = TRUE,
+                   dimnames = list(c(row_names),
+                                   c(col_names))))
+
+res_summary_b_0.5 <- 
+  res_summary_rep_b_0.5 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= SESOI),
+            N = n(),
+            pct_success = n_success/N * 100) %>% 
+  mutate(orig_ss = df_combined$orig_ss,
+         rep_sample_size = df_combined$rep_sample_size_b_0.5,
+         es_true = 0,
+         sample_size_approach = "b_0.5",
+         project = df_combined$project,
+         scenario = "null_effect")
+
+
+res_summary_b_0.5 <- left_join(df_combined, res_summary_b_0.5)
+
+
+res_summary_b_0.5_null <- res_summary_b_0.5
+
+save(res_summary_b_0.5_null, file = "./datasets/res_summary_b_0.5_null.RData")
+
+
+
+
+#################################
+### Simulate replication study ###
+### SCENARIO 3: S - error   ######
+##################################
+
+set.seed(84335)
+
+# first use replication sample size with SESOI = 0.5
+list_rep_data <- 
+  
+  foreach(study_id = study_id_vector) %do% {
+    
+    rep_data <- list()
+    
+    for(i in 1:n_exp) {
+      
+      rep_data[[i]] <- 
+        generate_study(ES_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]),
+                       sample_size = df_combined$rep_sample_size_b_0.5[study_id])
+      
+      rep_data[[i]] <-
+        rep_data[[i]] %>% 
+        mutate(study_id = study_id_vector[study_id],
+               ES_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]))
+      
+    }
+    
+    list_rep_data <- rep_data
+    
+  }
+
+rep_data_summary <- list()
+
+plan(multisession, workers = 10)
+for (i in 1:length(study_id_vector)) {
+  
+  rep_data_summary[[i]] <- 
+    future_map(list_rep_data[[i]], get_summary_study_rep)
+  
+}
+
+
+row_names <- NULL
+col_names <- c("study_id", "p_value", "effect")
+
+res_summary_rep_b_0.5 <- 
+  as_tibble(matrix(unlist(rep_data_summary),
+                   nrow = n_exp * length(study_id_vector), byrow = TRUE,
+                   dimnames = list(c(row_names),
+                                   c(col_names))))
+
+res_summary_b_0.5 <- 
+  res_summary_rep_b_0.5 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= 0.5),
+            N = n(),
+            pct_success = n_success/N * 100) %>% 
+  mutate(orig_ss = df_combined$orig_ss,
+         rep_sample_size = df_combined$rep_sample_size_b_0.5,
+         es_true = df_combined$orig_d - (1.25 * df_combined$orig_d),
+         sample_size_approach = "b_0.5",
+         project = df_combined$project,
          scenario = "s_error")
 
-res_summary_c <- left_join(df_combined, res_summary_c)
+
+res_summary_b_0.5 <- left_join(df_combined, res_summary_b_0.5)
 
 
-save(res_summary_c, file = "./datasets/res_summary_c_s_error.RData")
+res_summary_b_0.5_s_error <- res_summary_b_0.5
 
+save(res_summary_b_0.5_s_error, file = "./datasets/res_summary_b_0.5_s_error.RData")
+
+res_summary_b_0.5 <- 
+  bind_rows(res_summary_b_0.5_m_error, 
+            res_summary_b_0.5_null, 
+            res_summary_b_0.5_s_err)
 
 
--- scripts/simulation/sim_approach_b_1.R
+++ scripts/simulation/sim_approach_b_1.R
@@ -1,22 +1,25 @@
 ###########################################################
 #
-#   safeguard power analysis: 
-#   Replication powered lower 80% confidence interval 
-# 
+#   B_1: Replication powered for a SESOI = 1 
 #
 ###########################################################
 
 
 
 # source packages and additional functions
-source("../scripts/simulation/functions_for_simulation.R")
-source("../scripts/data_wrangling/load_packages.R")
-
+source("./scripts/simulation/functions_for_simulation.R")
+source("./scripts/data_wrangling/load_packages.R")
 
 # load combined data of all three replication projects
 load("./datasets/df_combined.RData")
+
 # calculate sample size for replication
 
+# Approach B:
+# Replication study powered at 50% for the smallest effect size of interest (SESOI)
+# SESOI 1 = 1
+
+# set SESOI to 1
 
 # add column study_id to loop over later
 df_combined <-
@@ -25,49 +28,21 @@ df_combined <-
   select(study_id, everything())
 
 
-# Approach C:
-# Replication study powered at 80% for the lower 80% confidence bound 
-# obtained from the original study
-# if lower CI bound < 0, the rep_sample_size_c column will display NA
+SESOI <- 1 
 
-# use des() function from compute.es package to compute CI around orig_d
-# ci_80_low is the lower bound of the 80% CI
-
-df_combined$ci_80_low <- NULL
+rep_sample_size_b_1 <- NULL
 
 for (i in 1:nrow(df_combined)) {
   
-  df_combined$ci_80_low[i] <-
-    
-    des(d =df_combined$ orig_d[i], 
-        n.1 = df_combined$orig_ss[i] / 2, 
-        n.2 = df_combined$orig_ss[i] / 2,
-        level = 80)$l.d
+  rep_sample_size_b_1[i] <-
+    ceiling(sample_size_b(data = df_combined[i, ],
+                          SESOI = SESOI,
+                          power = 0.5))
   
 }
 
-summary(df_combined$ci_80_low)
+df_combined$rep_sample_size_b_1 <- rep_sample_size_b_1 * 2
 
-# now compute replication sample size with the lower 80% confidence bound
-rep_sample_size_c <- NULL
-
-for (i in 1:nrow(df_combined)) {
-  
-  if (df_combined$ci_80_low[i] > 0) {
-    
-    rep_sample_size_c[i] <-
-      ceiling(sample_size_c(data = df_combined[i, ],
-                            power = 0.8))
-    
-  } else {
-    
-    rep_sample_size_c[i] <- NA
-    
-  }
-  
-}
-
-df_combined$rep_sample_size_c <- rep_sample_size_c * 2
 
 
 ################################
@@ -76,8 +51,13 @@ df_combined$rep_sample_size_c <- rep_sample_size_c * 2
 #
 ################################
 
-vec_not_nec <- ifelse(df_combined$rep_sample_size_c < 4, TRUE, FALSE)
-vec_unfeasible <- ifelse(df_combined$rep_sample_size_c >= 280 | df_combined$ci_80_low < 0, TRUE, FALSE)
+sum(df_combined$orig_ci_high < 1)
+sum(df_combined$orig_ci_low < 1 & df_combined$orig_ci_high>=1)
+sum(df_combined$orig_ci_low > 1 & df_combined$orig_ci_high > 1)
+
+
+vec_not_nec <- ifelse(df_combined$rep_sample_size_b_1 < 4, TRUE, FALSE)
+vec_unfeasible <- ifelse(df_combined$rep_sample_size_b_1 >= 280 | df_combined$orig_ci_high < SESOI, TRUE, FALSE)
 vec_go <- ifelse(vec_not_nec == FALSE & vec_unfeasible == FALSE, TRUE, FALSE)
 
 vec_conducted <- vector(length = nrow(df_combined))
@@ -87,30 +67,36 @@ vec_conducted[which(vec_not_nec == TRUE)] <- "not_necessary"
 
 df_combined$conducted <- vec_conducted
 
-decision1_c <- 
-  df_combined%>%
-  group_by(conducted)%>%
-  dplyr::summarize(n = n())%>%
-  mutate(approach = "safeguard")
-decision1_c
+decision1_b_1 <- df_combined%>%group_by(conducted)%>%dplyr::summarize(n = n())%>%mutate(approach = "SESOI_1")
+decision1_b_1
 
+
+####################################
+#
+#
+#     Simulate replication studies for 
+#     chosen experiments 
+#
+#####################################
 
 
 ##################################
 ### Simulate replication study ###
-### SCENARIO 1: M - error ########
+### SCENARIO 1: M-error ##########
 ##################################
-
-
 
 # set seed to reproduce results
 set.seed(84335)
 
 # number of experiments we run for each true underlying effect size
-n_exp <- 3
+n_exp <- 2
 
+# first use replication sample size with 95% power
+
+# for simulation only take significant results
 study_id_vector <- which(df_combined$conducted == "yes")
 
+# first use replication sample size with SESOI = 1
 list_rep_data <- 
   
   foreach(study_id = study_id_vector) %do% {
@@ -121,7 +107,7 @@ list_rep_data <-
       
       rep_data[[i]] <- 
         generate_study(df_combined$orig_d[study_id] / 2,
-                       sample_size = df_combined$rep_sample_size_c[study_id])
+                       sample_size = df_combined$rep_sample_size_b_1[study_id])
       
       rep_data[[i]] <-
         rep_data[[i]] %>% 
@@ -136,7 +122,7 @@ list_rep_data <-
 
 rep_data_summary <- list()
 
-plan(multisession)
+plan(multisession, workers = 10)
 for (i in 1:length(study_id_vector)) {
   
   rep_data_summary[[i]] <- 
@@ -144,42 +130,45 @@ for (i in 1:length(study_id_vector)) {
   
 }
 
+# rep_data_summary[[1]]
+
 row_names <- NULL
 col_names <- c("study_id", "p_value", "effect")
 
-res_summary_rep_c <-
+res_summary_rep_b_1 <- 
   as_tibble(matrix(unlist(rep_data_summary),
                    nrow = n_exp * length(study_id_vector), byrow = TRUE,
                    dimnames = list(c(row_names),
                                    c(col_names))))
 
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
+res_summary_b_1 <- 
+  res_summary_rep_b_1 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= SESOI),
             N = n(),
-            pct_success = n_success/N * 100) %>%
+            pct_success = n_success/N * 100) %>% 
   mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
+         rep_sample_size = df_combined$rep_sample_size_b_1[study_id_vector],
          es_true = df_combined$orig_d[study_id_vector] / 2,
-         sample_size_approach = "c",
+         sample_size_approach = "b_1",
          project = df_combined$project[study_id_vector],
          scenario = "m_error")
 
-res_summary_c <- left_join(df_combined, res_summary_c)
+res_summary_b_1 <- left_join(df_combined, res_summary_b_1)
 
-
-
-save(res_summary_c, file = "./datasets/res_summary_c_m_error.RData")
+save(res_summary_b_1, file = "./datasets/res_summary_b_1_m_error.RData")
 
 
 ##################################
 ### Simulate replication study ###
-### SCENARIO 2: null effect ######
+### SCENARIO 2: Null-effect ######
 ##################################
 
 set.seed(84335)
 
+n_exp <- 3
+
+# first use replication sample size with SESOI = 1
 list_rep_data <- 
   
   foreach(study_id = study_id_vector) %do% {
@@ -189,8 +178,8 @@ list_rep_data <-
     for(i in 1:n_exp) {
       
       rep_data[[i]] <- 
-        generate_study(0,
-                       sample_size = df_combined$rep_sample_size_c[study_id])
+        generate_study(ES_true = 0,
+                       sample_size = df_combined$rep_sample_size_b_1[study_id])
       
       rep_data[[i]] <-
         rep_data[[i]] %>% 
@@ -205,50 +194,59 @@ list_rep_data <-
 
 rep_data_summary <- list()
 
-plan(multisession, workers = 10) 
+plan(multisession, workers = 10)
 for (i in 1:length(study_id_vector)) {
   
   rep_data_summary[[i]] <- 
     future_map(list_rep_data[[i]], get_summary_study_rep)
   
 }
+
 # rep_data_summary[[1]]
 
 row_names <- NULL
 col_names <- c("study_id", "p_value", "effect")
 
-res_summary_rep_c <-
+res_summary_rep_b_1 <- 
   as_tibble(matrix(unlist(rep_data_summary),
                    nrow = n_exp * length(study_id_vector), byrow = TRUE,
                    dimnames = list(c(row_names),
                                    c(col_names))))
 
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
+res_summary_b_1 <- 
+  res_summary_rep_b_1 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= SESOI),
             N = n(),
-            pct_success = n_success/N * 100) %>%
+            pct_success = n_success/N * 100) %>% 
   mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
+         rep_sample_size = df_combined$rep_sample_size_b_1[study_id_vector],
          es_true = 0,
-         sample_size_approach = "c",
+         sample_size_approach = "b_1",
          project = df_combined$project[study_id_vector],
          scenario = "null_effect")
 
-res_summary_c <- left_join(df_combined, res_summary_c)
+
+res_summary_b_1 <- left_join(df_combined, res_summary_b_1)
 
 
-save(res_summary_c, file = "./datasets/res_summary_c_null.RData")
+res_summary_b_1_null <- res_summary_b_1
+
+save(res_summary_b_1_null, file = "./datasets/res_summary_b_1_null.RData")
 
 
-##################################
+
+
+#################################
 ### Simulate replication study ###
-### SCENARIO 3: s- error #########
+### SCENARIO 3: S - error   ######
 ##################################
 
 set.seed(84335)
 
+n_exp <- 2
+
+# first use replication sample size with SESOI = 1
 list_rep_data <- 
   
   foreach(study_id = study_id_vector) %do% {
@@ -259,7 +257,7 @@ list_rep_data <-
       
       rep_data[[i]] <- 
         generate_study(ES_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]),
-                       sample_size = df_combined$rep_sample_size_c[study_id])
+                       sample_size = df_combined$rep_sample_size_b_1[study_id])
       
       rep_data[[i]] <-
         rep_data[[i]] %>% 
@@ -272,7 +270,6 @@ list_rep_data <-
     
   }
 
-
 rep_data_summary <- list()
 
 plan(multisession, workers = 10)
@@ -283,32 +280,36 @@ for (i in 1:length(study_id_vector)) {
   
 }
 
+
 row_names <- NULL
 col_names <- c("study_id", "p_value", "effect")
 
-res_summary_rep_c <-
+res_summary_rep_b_1 <- 
   as_tibble(matrix(unlist(rep_data_summary),
                    nrow = n_exp * length(study_id_vector), byrow = TRUE,
                    dimnames = list(c(row_names),
                                    c(col_names))))
 
-res_summary_c <-
-  res_summary_rep_c %>%
-  group_by(study_id) %>%
-  summarize(n_success = sum(p_value <= 0.05),
+res_summary_b_1 <- 
+  res_summary_rep_b_1 %>% 
+  group_by(study_id) %>% 
+  summarize(n_success = sum(p_value <= 0.05 & effect >= 0.5),
             N = n(),
-            pct_success = n_success/N * 100) %>%
-  mutate(orig_ss = df_combined$orig_ss[study_id_vector],
-         rep_sample_size = df_combined$rep_sample_size_c[study_id_vector],
-         es_true = df_combined$orig_d[study_id] - (1.25 * df_combined$orig_d[study_id]),
-         sample_size_approach = "c",
-         project = df_combined$project[study_id_vector],
+            pct_success = n_success/N * 100) %>% 
+  mutate(orig_ss = df_combined$orig_ss,
+         rep_sample_size = df_combined$rep_sample_size_b_1,
+         es_true = df_combined$orig_d - (1.25 * df_combined$orig_d),
+         sample_size_approach = "b_1",
+         project = df_combined$project,
          scenario = "s_error")
 
-res_summary_c <- left_join(df_combined, res_summary_c)
+
+res_summary_b_1 <- left_join(df_combined, res_summary_b_1)
 
 
-save(res_summary_c, file = "./datasets/res_summary_c_s_error.RData")
+res_summary_b_1_s_error <- res_summary_b_1
+
+save(res_summary_b_1_s_error, file = "./datasets/res_summary_b_1_s_error.RData")
 
 
 
